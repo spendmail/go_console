da56321c83e81552daae65464eb4c924066f2e2b
spendmail <info@spendmail.ru>
2021-02-18 11:57:08 +0700

    refactoring dirs

--- go_console.py
+++ go_console.py
@@ -4,6 +4,9 @@ import pathlib
 import sys
 import click
 
+GO_DIRS = ['api', 'cmd', 'pkg', 'internal/domain/errors', 'internal/domain/interfaces', 'internal/domain/models',
+           'internal/domain/services', 'internal/adapters/grpc/api', 'internal/adapters/maindb', 'sql']
+
 
 @click.group()
 @click.version_option("0.0.1")
@@ -23,18 +26,8 @@ def make_dirs(path):
     if not os.path.exists(path):
         pathlib.Path(path)
 
-    pathlib.Path(os.path.join(path, "api")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "cmd")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "pkg")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "internal/domain/errors")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "internal/domain/interfaces")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "internal/domain/models")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "internal/domain/services")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "internal/adapters/grpc/api")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "internal/adapters/maindb")).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(os.path.join(path, "sql")).mkdir(parents=True, exist_ok=True)
-
-    pass
+    for go_dir in GO_DIRS:
+        pathlib.Path(os.path.join(path, go_dir)).mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == '__main__':
