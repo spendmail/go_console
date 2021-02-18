a1ba9df2165c2aaf0ad53abb0419ebf5aaa6ea2f
spendmail <info@spendmail.ru>
2021-02-18 17:42:12 +0700

    added readme.md and dockerfile

--- go_console.py
+++ go_console.py
@@ -7,6 +7,16 @@ import click
 GO_DIRS = ['api', 'cmd', 'pkg', 'internal/domain/errors', 'internal/domain/interfaces', 'internal/domain/models',
            'internal/domain/services', 'internal/adapters/grpc/api', 'internal/adapters/maindb', 'sql']
 
+DOCKERFILE = '''FROM golang:1.14
+
+WORKDIR /go/src/app
+COPY . .
+
+RUN go get -d -v ./...
+RUN go install -v ./...
+
+CMD ["app"]'''
+
 
 @click.group()
 @click.version_option("0.0.1")
@@ -28,6 +38,12 @@ def make_project(path):
 
     for go_dir in GO_DIRS:
         pathlib.Path(os.path.join(path, go_dir)).mkdir(parents=True, exist_ok=True)
+
+    with open(os.path.join(path, 'README.md'), 'a') as f:
+        f.write('# Project Name')
+
+    with open(os.path.join(path, 'DOCKERFILE'), 'a') as f:
+        f.write(DOCKERFILE)
 
 
 if __name__ == '__main__':
